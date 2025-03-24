f9af115e6b74c20dc9a49e74cd0ab7199c876423
miahafiz <miahafiz@kth.se>
2025-03-24 12:48:08 +0100

    first commit

---
+++ cpp_kernels/kernel_global_bandwidth/config.mk
@@ -0,0 +1,23 @@
+# By default, the value of ddr_banks is kept 2 
+# The ddr_banks should be chosen in such a way that the selected platform supports the DDR configuration.
+
+ddr_banks := 2
+
+# Kernel linker config files
+ifeq ($(findstring samsung, $(DEVICE)), samsung)
+ddr_banks := 1
+endif
+
+ifeq ($(findstring zc, $(DEVICE)), zc)
+VPP_LDFLAGS+= --config $(ddr_banks)bank_zc.cfg
+else
+ifeq ($(findstring samsung, $(DEVICE)), samsung)
+VPP_LDFLAGS+= --config samsung.cfg
+else
+VPP_LDFLAGS+= --config $(ddr_banks)bank.cfg
+endif
+endif
+
+VPP_FLAGS += -DNDDR_BANKS=$(ddr_banks)
+
+CXXFLAGS += -DNDDR_BANKS=$(ddr_banks)
